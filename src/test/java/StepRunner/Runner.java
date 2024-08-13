071ecd4fc807dd75acaf17578a6d3002953e3f5b
NitheeshM <mnitheesh301999@gmail.com>
2024-08-13 11:01:35 +0600

    plugin change

--- src/test/java/StepRunner/Runner.java
+++ src/test/java/StepRunner/Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features = "src/test/java/Features/ajio1.feature",
 		glue = "StepDef",
-		plugin={"html: extreport/cucumber.html"}
+		plugin={"html:extreport/cucumber.html"}
 		)
 
 public class Runner {
